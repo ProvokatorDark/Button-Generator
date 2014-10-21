29b5ccc371eec0316fb2343e5716776073e712c4
Boss <okknopka@gmail.com>
2014-10-21 07:41:20 +0600

    "add js app"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+(function(){
+    var app ={
+
+            var borderRad=this.create.css('border-radius'),
+                bgcolor=this.create.css('background-color'),
+                brcolor=this.create.css('border-color'),
+                codeResultArea=$('#code-result');
+
+
+            codeResultArea.text(
+                '-moz-border-radius:'+ borderRad+';\n'+
+                '-webkit-border-radius:'+ borderRad+';\n'+
+                'border-radius:'+ borderRad+';\n'+
+                'border-color:'+ brcolor+';\n'+
+                'background-color:'+bgcolor+';'
+            );
+        }
+
+    }
+    app.initialize();
+}());
